c73d39ec679e6ac81551301c0d650370434aa743
unknown <shreeya.gundreddy@gmail.com>
2026-01-04 12:39:17 -0600

    Add Terraform templates for Lambda and RDS

--- infra/main.tf
+++ infra/main.tf
@@ -12,3 +12,26 @@ provider "aws" {
 resource "aws_s3_bucket" "data_bucket" {
     bucket = var.bucket_name
 }
+# Lambda template 
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "day5-example-lambda"
+  runtime       = "python3.9"
+  handler       = "index.handler"
+
+  role = "arn:aws:iam::123456789012:role/lambda-execution-role"
+
+  filename = "lambda.zip"
+  # source_code_hash intentionally omitted to avoid CI failures
+}
+# RDS template 
+resource "aws_db_instance" "example_rds" {
+  identifier        = "day5-example-rds"
+  engine            = "postgres"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  username = "admin"
+  password = "password123"
+
+  skip_final_snapshot = true
+}
